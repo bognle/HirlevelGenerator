66478381b25d36dc0eb5e5aabab6c66748388fbc
Lehel Bognar <bognar.lehel@sze.hu>
2024-03-04 10:36:44 +0100

    2

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 from adatkiolvasas import generatePartOutPut, insert_part
 from selectInputFile import select_file
+import tkinter as tk
+from tkinter import filedialog
 
 
+try:
+    # Fájlok elérési útjainak beállítása
+    input_file_path = 'template/template.html'
+    input_part_file_path='template/part.html'
+
+    output_part_file_path =  'output_part.html'
+    output_file_name = 'output.html'
+    # Excel fájl elérési útja
+    excel_file = select_file()
+
+    #OutputPart generál
+    # ás
+    generatePartOutPut(input_part_file_path, excel_file)
+
+    # Beszúrás végrehajtása
+    insert_part(input_file_path, output_part_file_path, output_file_name)
 
 
-
-# Fájlok elérési útjainak beállítása
-input_file_path = './template/template.html'
-input_part_file_path='./template/part.html'
-
-output_part_file_path =  'output_part.html'
-output_file_name = 'output.html'
-# Excel fájl elérési útja
-excel_file = select_file()
-
-#OutputPart generál
-# ás
-generatePartOutPut(input_part_file_path, excel_file)
-
-# Beszúrás végrehajtása
-insert_part(input_file_path, output_part_file_path, output_file_name)
+except Exception as e:
+    # Ha valamilyen hiba történik, az kivételt dob, és ide kerülünk
+    print("Hiba történt:", e)  # Kiírjuk a hibaüzenetet a konzolra
+    root = tk.Tk()
+    root.withdraw()  # A gyökér ablakot elrejtjük
+    
+    folder_path = filedialog.askdirectory(message=e)  # Mappa kiválasztása ablak megjelenítése
 
--- setup.py
+++ setup.py
@@ -1,6 +1,30 @@
+#FOR MAC
+
+#from setuptools import setup
+#
+#setup(
+#    app=["main.py"],
+#    setup_requires=["py2app"],
+#    data_files=[('template', ['template/part.html', 'template/template.html'])]  # Itt add meg a template mappád tartalmát
+#)
+#
+
+
+
+#FOR WINDOWS
+
 from setuptools import setup
+import py2exe
 
 setup(
-    app=["main.py"],
-    setup_requires=["py2app"],
+    console=["main.py"],  # Módosítsuk console-ra, ha parancssori alkalmazást készítünk
+    options={
+        "py2exe": {
+            "packages": ["tkinter"],  # Ha a program használja a tkinter modult
+            "includes": ["tkinter", "tkinter.filedialog", "tkinter.messagebox"]  # Szükséges modulok listája
+        }
+    },
+    data_files=[
+        ('template', ['template/part.html', 'template/template.html'])
+    ]
 )
